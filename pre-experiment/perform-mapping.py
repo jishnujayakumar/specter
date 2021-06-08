680eb35830d08b92149596efc45a7b16d39c7ab8
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-06-08 11:39:03 +0530

    ADD mapping.txt -> similarity_scores mapper script

---
+++ pre-experiment/perform-mapping.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+mappingData = None
+dir = sys.argv[1]
+
+with open(f"{dir}/mapping.txt", "r") as mappingFile:
+    for line in mappingFile.readlines():
+        mappingData = line.strip().split(" : ")
+        frm = mappingData[0].replace("_", "\\_")
+        to = mappingData[1].replace("_", "\\_")
+        print(f"sed -i 's/{frm}/{to}/g' {dir}/similarity-scores.txt")
+        os.system(f"sed -i 's/{frm}/{to}/g' {dir}/similarity-scores.txt")
