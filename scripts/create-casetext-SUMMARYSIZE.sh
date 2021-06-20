81a3349f7d7b9126c6803afaa65b1829dd1cd05a
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-06-20 15:36:04 +0530

    FIX

--- scripts/create-casetext-SUMMARYSIZE.sh
+++ scripts/create-casetext-SUMMARYSIZE.sh
@@ -2,12 +2,13 @@
 # Usage: ./create-casetext-SUMMARYSIZE.sh legal-data-dsdr-summarized 512 (name of legal-data dir, summarySize)
 dir="$ELECTER_DIR/$1"
 outputF="$dir/file-to-summary-size.txt"
+inpDir="$dir/casetext"
 summaryTokenSize=$2
 
 rm outputF
 
-for casetextFile in `ls $dir/castext`;do
+for casetextFile in `ls $inpDir`;do
     echo "$casetextFile\t$summaryTokenSize" >> $outputF
 done
 
-mv $dir/castext $dir/original-castext-without-summarization
+mv $inpDir $dir/original-castext-without-summarization
