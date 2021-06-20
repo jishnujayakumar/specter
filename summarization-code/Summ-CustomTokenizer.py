d91002e8e32453ef51ab4fc9b1d49511bf82bc96
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-06-20 15:46:06 +0530

    FIX

--- summarization-code/Summ-CustomTokenizer.py
+++ summarization-code/Summ-CustomTokenizer.py
@@ -38,7 +38,7 @@ PATH = "$ELECTER_DIR/legal-data-dsdr-summarized/original-castext-without-summari
 OUTDIR = '$ELECTER_DIR/legal-data-dsdr-summarized/casetext'
 # SUMMARYSIZEJSON = open('file containing summary length in no.of words.txt',"r")
 #file containing summary length in no.of words.txt format : filename<tab>summary-len-count
-SUMMARYSIZEJSON = open("$ELECTER_DIR/legal-data-dsdr-summarized/file-to-summary-size.txt,"r")
+SUMMARYSIZEJSON = open("$ELECTER_DIR/legal-data-dsdr-summarized/file-to-summary-size.txt","r")
 
 AVGWORDPERSENT = 17
 
