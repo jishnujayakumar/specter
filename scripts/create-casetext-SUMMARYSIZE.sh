450b94b26ff21f4350c7ac3ac7f8d17624506a80
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-06-20 16:02:37 +0530

    FIX

--- scripts/create-casetext-SUMMARYSIZE.sh
+++ scripts/create-casetext-SUMMARYSIZE.sh
@@ -5,8 +5,10 @@ outputF="$dir/file-to-summary-size.txt"
 inpDir="$dir/casetext"
 summaryTokenSize=$2
 
+rm outputF
+
 for casetextFile in `ls $inpDir`;do
-    echo $casetextFile$'\t'$summaryTokenSize > $outputF
+    echo $casetextFile$'\t'$summaryTokenSize >> $outputF
 done
 
 mv $inpDir/ $dir/original-castext-without-summarization/
