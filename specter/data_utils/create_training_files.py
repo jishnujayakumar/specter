1b3c5724898344888b35a485bae9ffd162def8c8
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-07-03 15:58:08 +0530

    SAMPLES PER QUERY == 10

--- specter/data_utils/create_training_files.py
+++ specter/data_utils/create_training_files.py
@@ -496,7 +496,7 @@ if __name__ == '__main__':
     ap.add_argument('--njobs', help='number of parallel jobs for instance conversion', default=1, type=int)
     ap.add_argument('--njobs_raw', help='number of parallel jobs for triplet generation', default=12, type=int)
     ap.add_argument('--ratio_hard_negatives', default=0.3, type=float)
-    ap.add_argument('--samples_per_query', default=5, type=int)
+    ap.add_argument('--samples_per_query', default=10, type=int)
     ap.add_argument('--margin_fraction', default=0.5, type=float)
     ap.add_argument('--comment', default='', type=str)
     ap.add_argument('--data_files', help='space delimted list of data files to override default', default=None)
