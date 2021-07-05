4087eb6b55d3db9a058ed22430eb14c55e6b8fbe
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2021-07-05 13:10:25 +0530

    UPDATE

--- pre-experiment/get-legal-bert.py
+++ pre-experiment/get-legal-bert.py
@@ -49,5 +49,9 @@ def prepareLegalBertArtifacts(model):
 
 
 if __name__ == "__main__":
-    for model in ["zlucia/custom-legalbert"]:
+    for model in [
+        "zlucia/custom-legalbert",
+        "nlpaueb/legal-bert-base-uncased",
+        "nlpaueb/legal-bert-base-uncased"
+    ]:
         prepareLegalBertArtifacts(model)
